34ba927286ab6c5c7d2cab5fb14d2a0b34eac4de
Seba Carrasco Poblete <sebacarrasco93@gmail.com>
2024-11-18 04:21:51 -0300

    wip: register card

--- src/LaravelOneclick.php
+++ src/LaravelOneclick.php
@@ -2,4 +2,32 @@
 
 namespace SextaNet\LaravelOneclick;
 
-class LaravelOneclick {}
+use SextaNet\LaravelOneclick\Exceptions\MissingKeysInProduction;
+use Transbank\Webpay\Oneclick\MallInscription;
+
+class LaravelOneclick {
+    protected static function checkConfig(): void
+    {
+        if (! config('oneclick.commerce_code') || ! config('oneclick.secret')) {
+            throw new MissingKeysInProduction;
+        }
+    }
+
+    protected static function getResponseUrl(): string
+    {
+        return session()->get('response_url') ?? route('oneclick.response');
+    }
+
+    public static function setResponseUrl(string $response_url): void
+    {
+        session()->flash('response_url', $response_url);
+    }
+
+    public static function registerCard(string $username, string $email)
+    {
+        $response = (new MallInscription)
+            ->start($username, $email, self::getResponseUrl());
+
+        dd($response);
+    }
+}
